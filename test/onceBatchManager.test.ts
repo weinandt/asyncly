af34748610a6780b8ab18a5b3946e738a4f6a186
weinandt <nick.weinandt@gmail.com>
2022-02-21 07:48:01 -0600

    Added more OnceBatchManager tests

--- test/onceBatchManager.test.ts
+++ test/onceBatchManager.test.ts
@@ -55,4 +55,57 @@ describe('OnceBatchManager', () => {
         await assert.rejects(batch.addToBatchAndGetResult(0))
         await assert.doesNotReject(batch.addToBatchAndGetResult(0))
     })
+    it('Async function throwing synchronously will reject.', async () => {
+        // Arrange
+        // First time will throw, second time will not
+        const asyncFunction = async (batch: number[]) => {
+            throw new Error('Throwing synchronously')
+        }
+
+        const batch = new OnceBatchManager<number, any>({
+            maxSize: 1,
+            asyncFunction,
+        })
+
+        // Assert.
+        await assert.rejects(batch.addToBatchAndGetResult(0))
+    })
+    it('Batches can complete out of order.', async () => {
+        // Arrange
+        const fakeTimers = FakeTimers.install()
+        let numCalls = 0
+        let firstExecutionCompletedTime = 0
+        let secondExeutionCompletedTime = 0
+        const maxWaitTime = 1000
+        const asyncFunction = async (batch: number[]) => {
+            if (numCalls == 0) {
+                numCalls++
+                setTimeout(() => {
+                    firstExecutionCompletedTime = Date.now()
+                }, maxWaitTime)
+            }
+            else {
+                setTimeout(() => {
+                    secondExeutionCompletedTime = Date.now()
+                }, 0)
+            }
+        }
+
+        const batch = new OnceBatchManager<number, any>({
+            maxSize: 1,
+            asyncFunction,
+        })
+
+        // Act
+        const firstPromise = batch.addToBatchAndGetResult(0)
+        const secondPromise = batch.addToBatchAndGetResult(1)
+        await fakeTimers.tickAsync(maxWaitTime + 1)
+        await Promise.all([firstPromise, secondPromise])
+
+        // Assert
+        assert.equal(firstExecutionCompletedTime > secondExeutionCompletedTime, true, 'Second execution should have compelted first.')
+
+        // Clean Up
+        fakeTimers.uninstall()
+    })
 })
